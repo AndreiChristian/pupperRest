806a4140547afc4c35357ac1b683ed135696c6dc
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-06-12 14:55:03 +0200

    trying to fix the dynamic queries

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { getAllDogs } from "./all";
 import { getOne } from "./one";
+import { getFilteredDogs } from "./filter";
 
 const router = Router();
 
 router.get("/all", getAllDogs);
 router.get("/:id", getOne);
+router.get("/filter", getFilteredDogs);
 
 export default router;
