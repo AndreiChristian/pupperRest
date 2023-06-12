9746d1b074d6692b86a350caeed0adfcc822f5e1
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-06-12 15:37:29 +0200

    working on the dynamic query

--- src/handlers/filter.ts
+++ src/handlers/filter.ts
@@ -18,42 +18,50 @@ interface QueryParameters {
 export const getFilteredDogs = async (req: Request, res: Response) => {
   try {
     let queryParameters: QueryParameters = {
-      breed: req.query.breed as string,
-      size: req.query.size as string,
-      function: req.query.use as string,
-      purpose: req.query.purpose as string,
-      coat_type: req.query.coatType as string,
-      temperament: req.query.temperament as string,
-      origin: req.query.origin as string,
-      working_group: req.query.workingGroup as string,
-      life_expectancy: req.query.lifeExpectancy as string,
-      exercise_needs: req.query.exerciseNeeds as string,
-      trainability: req.query.trainability as string,
+      breed: (req.query.breed as string) || "",
+      size: (req.query.size as string) || "",
+      function: (req.query.use as string) || "",
+      purpose: (req.query.purpose as string) || "",
+      coat_type: (req.query.coatType as string) || "",
+      temperament: (req.query.temperament as string) || "",
+      origin: (req.query.origin as string) || "",
+      working_group: (req.query.workingGroup as string) || "",
+      life_expectancy: (req.query.lifeExpectancy as string) || "",
+      exercise_needs: (req.query.exerciseNeeds as string) || "",
+      trainability: (req.query.trainability as string) || "",
     };
 
-    // Remove undefined parameters
-    queryParameters = Object.entries(
-      Object.entries(queryParameters).filter(([_, v]) => v != null)
-    ) as QueryParameters;
+    // initializing the base query
 
-    let baseQuery = `SELECT * FROM dogs WHERE `;
-    let values: string[] = [];
-    let valueCounter = 1;
+    let query = "SELECT * FROM dogs WHERE"; //query
+    const values: string[] = []; //values
+    let counter = 1;
 
-    for (let param in queryParameters) {
-      baseQuery += `${param} = $${valueCounter} AND `;
-      values.push(queryParameters[param as keyof QueryParameters] as string);
-      valueCounter++;
+    // get the query params
+
+    Object.entries(queryParameters).forEach(([index, value]) => {
+      if (value) {
+        while (value.includes("\n")) {
+          value = value.slice(0, -1);
+        }
+
+        query += ` ${index} = $${counter} AND`;
+        counter++;
+        values.push(value);
+      }
+    });
+
+    query = query.slice(0, -3);
+
+    const { rows } = await db.query(query, values);
+
+    console.table(values);
+
+    if (!rows) {
+      throw new Error("Error fetching data");
     }
 
-    baseQuery = baseQuery.slice(0, -4); // remove the last 'AND '
-
-    // const result = await db.query(baseQuery, values);
-    // res.json(result.rows);
-
-    console.log(baseQuery);
-
-    res.json("Hello");
+    res.json(rows);
   } catch (err) {
     console.error("Error executing query:", err);
     res.status(500).json({ error: "An error occurred while fetching data" });
--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -6,7 +6,7 @@ import { getFilteredDogs } from "./filter";
 const router = Router();
 
 router.get("/all", getAllDogs);
-router.get("/:id", getOne);
 router.get("/filter", getFilteredDogs);
+router.get("/:id", getOne);
 
 export default router;
