5d7637b07becc2b81393cc1802b364992bf7cc30
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-06-12 17:37:08 +0200

    working on the documentation

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,23 @@ import express from "express";
 import { config } from "dotenv";
 
 import apiRouter from "./handlers/index";
+import documentationRouter from "./handlers/documentation";
 import { cors } from "./middleware/cors";
 import { perHourLimiter, perSecondLimiter } from "./middleware/throttle";
+import path from "path";
 
 config();
 const port = process.env.PORT || 3333;
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(cors);
 app.use(perSecondLimiter);
 app.use(perHourLimiter);
 
+app.use("/docs", documentationRouter);
 app.use("/api", apiRouter);
 
 app.listen(port, () => {
